7ea12dae43329d7fd52f689dc522a0de0171b0ce
patrick ojunde <patrickojunde@gmail.com>
2020-11-14 18:55:05 +0100

    completed chapter 4

---
+++ chp4-18/src/main/java/chp4_arrays_collections/Activity2.java
@@ -0,0 +1,26 @@
+package chp4_arrays_collections;
+
+public class Activity2 {
+
+    /*
+    Iterating through Large Lists
+
+In contemporary computing, we deal with large sets of data. The purpose of this activity is to create a random-sized list of random numbers to perform some basic operations on data, such as obtaining the average.
+
+    To start, you should create a random list of numbers.
+    To compute the average, you could create an iterator that will go through the list of values and add the weighted value corresponding to each element.
+    The value coming from the iterator.next() method must be cast into a double before it can be weighed against the total number of elements.
+
+    If you've implemented everything properly, the results of the averaging should similar to:
+
+    Total amount of numbers: 3246
+    Average: 49.785278826074396
+
+    Or, it could be:
+
+    Total amount of numbers: 6475
+    Average: 50.3373892275651
+     */
+
+
+}
---
+++ chp4-18/src/main/java/chp4_arrays_collections/Properties_.java
@@ -0,0 +1,81 @@
+package chp4_arrays_collections;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.Set;
+
+public class Properties_ {
+
+    /*Properties in the collections framework are used to maintain list of
+    key-value pairs where both are of the String class. Properties are relevant
+    when obtaining environmental values from the operating system,
+    for example, and are the grounding class for many other classes.
+
+    One of the main characteristics of the Properties class is that it allows
+    the definition of a default response
+    in the case of a search for a certain key not being satisfactory.
+    * */
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        Set setOfKeys; String key;
+
+        properties.put("Os", "Ubuntu Linux");
+        properties.put("Version", "18.04");
+        properties.put("Language", "English (UK)");
+
+        //iterate through the map
+        Iterator keyIterator = properties.keySet().iterator();
+
+
+        while (keyIterator.hasNext()) {
+            key = keyIterator.next().toString();
+            System.out.printf("Key = %s%n", key,
+                    properties.getProperty(key));
+
+            System.out.printf("Key %s value = %s%n",
+                    key ,properties.getProperty(key));
+        }
+
+        /* Another interesting method to be found in the
+        properties class is the list (); it comes with two different implementation
+        that allows for sending contents of a list to different data handlers.
+
+        We can stream the whole properties
+        list to a PrintStreamer object such as System.out.
+        This offers a simple way of displaying what is in a list without having to iterate through it.
+   * */
+
+        System.out.println("Printing With System.out stream (output stream)");
+        properties.list(System.out);
+
+        //The propertyNames() method returns an Enumeration list,
+        //and by iterating through it, we will obtain the keys to the whole list.
+        //This is an alternative to creating a set and running the KeySet() method
+
+
+        Enumeration enumeration = properties.propertyNames();
+
+        System.out.println("Printing With Enumeration");
+        while (enumeration.hasMoreElements())
+            System.out.println(enumeration.nextElement());
+
+
+        //We also have the setProperty  method which is used to
+        // modify the value of an existing key, or will eventually
+        //create a new kwy-value pair of the key is not found.
+        //The method will answer with the old value if the key exists, or answer with code otherwise
+
+        String oldValue = (String) properties.setProperty("Language", "German");
+
+        if(!(oldValue==null)) System.out.println("Modified the language Property");
+
+
+        properties.list(System.out);
+
+
+        //NB: There are more methods in the Properties class that deals with
+        //storing retrieving lists of properties to/from files.
+    }
+}
